ebc1458df2bd851d7940bb9f93adbf575fe031bd
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-08-30 13:25:09 +0200

    suffixes for all operating systems

---
+++ crates/steam_deltarune/src/sufs.rs
@@ -0,0 +1,7 @@
+pub const WINDOWS: &str = r"AppData\Local\DELTARUNE";
+pub const MACOS: &str = "Library/Application Support/com.tobyfox.deltarune";
+
+pub const LINUX_COMPAT: &str =
+    "steamapps/compatdata/1690940/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";
+pub const LINUX_FLATPAK: &str = ".var/app/com.valvesoftware.Steam/data/Steam/steamapps/compatdata/1690940/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";
+pub const LINUX_NATIVE: &str = ".local/share/Steam/steamapps/compatdata/1690940/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";

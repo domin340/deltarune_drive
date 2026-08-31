061c08d7234190d157ef0a32641a2cde6f5c8eb0
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-08-31 14:14:44 +0200

    sorted imports

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -1,20 +1,13 @@
-use std::io;
-
-use ratatui::DefaultTerminal;
-
 mod backups;
 mod conf;
+
+use ratatui::DefaultTerminal;
+use std::io;
 
 fn main() -> io::Result<()> {
     ratatui::run(run_app)?;
     Ok(())
 }
-
-/*
-- store the backups and the settings inside local data directory
-- if deltarune path could not be found ask the user where the directory is located with the deltarune local data
-    - store the location afterwards
-*/
 
 fn run_app(term: &mut DefaultTerminal) -> io::Result<()> {
     Ok(())

40d3806ae7e5051084f02b00ca81a0aec133d1d6
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-03 16:25:17 +0200

    Corners -> CornerIndices, better fitting name

--- crates/tui/src/my_widgets/mod.rs
+++ crates/tui/src/my_widgets/mod.rs
@@ -3,20 +3,22 @@ use ratatui::layout::{Position, Rect};
 pub mod button;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Corners {
+pub struct CornerIndices {
     pub top_left: Position,
     pub top_right: Position,
     pub bottom_left: Position,
     pub bottom_right: Position,
 }
 
-impl From<Rect> for Corners {
+impl From<Rect> for CornerIndices {
     fn from(rect: Rect) -> Self {
+        let x_right_idx = rect.x + rect.width - 1;
+        let y_bottom_idx = rect.y + rect.height - 1;
         Self {
             top_left: Position::new(rect.x, rect.y),
-            top_right: Position::new(rect.x + rect.width, rect.y),
-            bottom_left: Position::new(rect.x, rect.y + rect.height),
-            bottom_right: Position::new(rect.x + rect.width, rect.y + rect.height),
+            top_right: Position::new(x_right_idx, rect.y),
+            bottom_left: Position::new(rect.x, y_bottom_idx),
+            bottom_right: Position::new(x_right_idx, y_bottom_idx),
         }
     }
 }

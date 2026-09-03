76d3530213814dbec3ce981eb807da2fa665cced
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-03 15:39:49 +0200

    corners struct

--- crates/tui/src/my_widgets/mod.rs
+++ crates/tui/src/my_widgets/mod.rs
@@ -1,1 +1,22 @@
+use ratatui::layout::{Position, Rect};
+
 pub mod button;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Corners {
+    pub top_left: Position,
+    pub top_right: Position,
+    pub bottom_left: Position,
+    pub bottom_right: Position,
+}
+
+impl From<Rect> for Corners {
+    fn from(rect: Rect) -> Self {
+        Self {
+            top_left: Position::new(rect.x, rect.y),
+            top_right: Position::new(rect.x + rect.width, rect.y),
+            bottom_left: Position::new(rect.x, rect.y + rect.height),
+            bottom_right: Position::new(rect.x + rect.width, rect.y + rect.height),
+        }
+    }
+}

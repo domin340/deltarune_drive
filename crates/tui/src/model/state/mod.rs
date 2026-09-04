1e8e1a1db97958ab200aeddcdd2ecda9cd2d1f39
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-04 20:42:43 +0200

    from_conf and derived Default

--- crates/tui/src/model/state/mod.rs
+++ crates/tui/src/model/state/mod.rs
@@ -4,9 +4,19 @@ mod render;
 use crate::model::conf::Conf;
 pub use manage_focus::{BkpPageFocus, ExplorerFocus, ExplorerListItem, Focus, UiAction};
 
+#[derive(Default)]
 pub struct State {
     /// stores all the backups and handles the IO inside the local data directory.
     pub conf: Conf,
     pub focus: Focus,
     pub list_item: Option<ExplorerListItem>,
+}
+
+impl State {
+    pub fn from_conf(conf: Conf) -> Self {
+        Self {
+            conf,
+            ..Default::default()
+        }
+    }
 }

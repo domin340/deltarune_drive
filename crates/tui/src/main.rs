81434503721f0980374de8006d60cf014b6f8f03
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-04 19:30:53 +0200

    glue (new focus, fake bkps)

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -4,16 +4,18 @@ mod focus_manager;
 mod my_widgets;
 
 use crate::{
+    backups::Bkp,
     conf::{Conf, extend_bkps_with_fakes},
+    focus_manager::{Focus, FocusManager, UiAction},
     my_widgets::button::{Button, ButtonState},
 };
 use crossterm::event::{self, KeyCode};
 use ratatui::{
     DefaultTerminal, Frame,
-    layout::{Constraint, Layout},
-    style::{Color, Style},
+    layout::{Constraint, Layout, Offset, Rect},
+    style::{Color, Modifier, Style},
     text::Line,
-    widgets::Block,
+    widgets::{Block, List, ListState},
 };
 use std::io;
 
@@ -23,11 +25,11 @@ fn main() -> io::Result<()> {
 }
 
 fn run_app(term: &mut DefaultTerminal) -> io::Result<()> {
-    let state = State {
+    let mut state = State {
         // conf: Conf::try_load().unwrap_or_default(),
         // temporary line
         conf: extend_bkps_with_fakes(Conf::default()),
-        focus: Focus::Explorer(ExplorerItemFocus(0)),
+        focus: FocusManager::default(),
     };
 
     'run_app: loop {
@@ -36,7 +38,11 @@ fn run_app(term: &mut DefaultTerminal) -> io::Result<()> {
         if let Some(key) = event::read()?.as_key_press_event() {
             match key.code {
                 KeyCode::Char('q') => break 'run_app,
-                _ => {}
+                _ => {
+                    if let Some(ui_action) = UiAction::parse(key.code) {
+                        state.focus.exec(ui_action);
+                    }
+                }
             }
         }
     }
@@ -44,76 +50,80 @@ fn run_app(term: &mut DefaultTerminal) -> io::Result<()> {
     Ok(())
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct ExplorerItemFocus(usize);
-
-impl ExplorerItemFocus {
-    fn item_idx(&self) -> usize {
-        self.0
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum Focus {
-    None,
-    Explorer(ExplorerItemFocus),
-    RightPanel,
-}
-
-impl Focus {
-    fn is_explorer(&self) -> bool {
-        matches!(self, Focus::Explorer(_))
-    }
-
-    fn is_right_panel(&self) -> bool {
-        self == &Focus::RightPanel
-    }
-}
-
 struct State {
     /// stores all the backups and handles the IO inside the local data directory.
     conf: Conf,
-    focus: Focus,
+    // focus: Focus,
+    focus: FocusManager,
 }
 
 impl State {
+    fn is_explorer_focused(&self) -> bool {
+        matches!(self.focus.focus(), Focus::Explorer(_))
+    }
+
+    fn is_bkp_page_focused(&self) -> bool {
+        matches!(self.focus.focus(), Focus::BkpPage(_))
+    }
+
     fn ui(&self, frame: &mut Frame) {
-        let [explorer_area, right_panel_area] =
+        let [explorer_area, bkp_page_area] =
             Layout::horizontal([Constraint::Percentage(40), Constraint::Percentage(60)])
                 .areas(frame.area());
 
         // == handle explorer here ==
         let explorer_block = {
-            let mut block = Block::bordered().title("Explorer");
-            if self.focus.is_explorer() {
+            let mut block = Block::bordered().title("Explorer (LEFT)");
+            if self.is_explorer_focused() {
                 block = block.border_style(Style::default().fg(Color::Blue));
             }
 
             block
         };
 
-        let [explorer_new_button_area, explorer_list_area] =
-            Layout::vertical([Constraint::Length(3), Constraint::Fill(1)])
-                .areas(explorer_block.inner(explorer_area));
+        let [
+            explorer_new_button_area,
+            explorer_list_label,
+            explorer_list_area,
+        ] = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Length(1),
+            Constraint::Fill(1),
+        ])
+        .areas(explorer_block.inner(explorer_area));
 
+        frame.render_widget(explorer_block, explorer_area);
         frame.render_stateful_widget(
             // here make the button secondary (find colors for the theme)
             Button::new(Line::from("New").centered()),
             explorer_new_button_area,
             &mut ButtonState::default(),
         );
-        frame.render_widget(explorer_block, explorer_area);
+        frame.render_widget(Line::from("[BACKUP LIST]").centered(), explorer_list_label);
+        self.bkp_list(explorer_list_area, frame);
 
         // == handle right panel here ==
-        let right_panel_block = {
-            let mut block = Block::bordered().title("Display");
-            if self.focus.is_right_panel() {
+        let bkp_page_block = {
+            let mut block = Block::bordered().title("Display (RIGHT)");
+            if self.is_bkp_page_focused() {
                 block = block.border_style(Style::default().fg(Color::Blue));
             }
 
             block
         };
 
-        frame.render_widget(right_panel_block, right_panel_area);
+        frame.render_widget(bkp_page_block, bkp_page_area);
+    }
+
+    fn bkp_list(&self, area: Rect, frame: &mut Frame) {
+        frame.render_stateful_widget(
+            List::new(self.bkp_names()).highlight_style(Modifier::REVERSED),
+            area,
+            &mut ListState::default().with_selected(self.focus.list_item_idx()),
+        );
+    }
+
+    fn bkp_names(&self) -> impl Iterator<Item = &str> {
+        self.conf.bkps.iter().map(Bkp::name)
     }
 }

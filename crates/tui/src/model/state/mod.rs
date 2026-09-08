9bed4ad6f42a0f50a5bfe9df858225cf147857d2
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-08 17:59:50 +0200

    removed LinesString struct from before

--- crates/tui/src/model/state/mod.rs
+++ crates/tui/src/model/state/mod.rs
@@ -7,48 +7,6 @@ use crate::{
 };
 pub use manage_focus::{ExplorerListItem, Focus, UiAction};
 
-/// String divided into lines.
-#[derive(Default)]
-pub struct LinesString(pub Vec<String>);
-
-impl LinesString {
-    pub fn from_vec(v: Vec<String>) -> Self {
-        Self(v)
-    }
-
-    pub fn from_iter(v: impl Iterator<Item = impl Into<String>>) -> Self {
-        Self::from_vec(v.map(Into::into).collect())
-    }
-
-    pub fn lines(&self) -> impl Iterator<Item = &str> {
-        self.0.iter().map(String::as_str)
-    }
-
-    pub const fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    pub const fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-
-    pub fn get_line(&self, line: usize) -> Option<&str> {
-        self.0.get(line).map(String::as_str)
-    }
-}
-
-impl ToString for LinesString {
-    fn to_string(&self) -> String {
-        self.0.join("\n")
-    }
-}
-
-impl From<String> for LinesString {
-    fn from(value: String) -> Self {
-        Self(value.split('\n').map(String::from).collect())
-    }
-}
-
 #[derive(Default)]
 pub struct State {
     /// stores all the backups and handles the IO inside the local data directory.

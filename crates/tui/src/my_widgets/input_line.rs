9616ca52d8f4e65c8fcc956d10ae499e60eafc88
DOMIN1310 <96619090+domin340@users.noreply.github.com>
2026-09-24 19:38:33 +0200

    with_placeholder

--- crates/tui/src/my_widgets/input_line.rs
+++ crates/tui/src/my_widgets/input_line.rs
@@ -88,7 +88,7 @@ impl<'line> Input<'line> {
         }
     }
 
-    pub const fn placeholder(mut self, s: &'line str) -> Self {
+    pub const fn with_placeholder(mut self, s: &'line str) -> Self {
         if self.s.is_empty() {
             self.s = s;
         }
